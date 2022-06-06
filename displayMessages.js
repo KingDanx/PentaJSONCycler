f4412712efee1350832b27eeb8514a65a65bc843
Dan Prudhomme <dannyprud@yahoo.com>
2022-06-06 05:12:12 -0500

    Clean up code.

--- displayMessages.js
+++ displayMessages.js
@@ -1,32 +1,16 @@
-import messages from './messages.json' assert {type: 'json'};
+import messages from "./messages.json" assert { type: "json" };
 
-let message;
-
-function displayMessage(jsonArray){
-
-    for (let i = 0; i < jsonArray.messages.length; i++) {
-        if(i == 0){
-             console.log(`Message: ${jsonArray.messages[i].text}`);
-             document.getElementById('message').innerHTML = jsonArray.messages[i].text;
-        }
-        else{
-            let j = 1;
-            function myLoop() { 
-                setTimeout(function() {   
-                    document.getElementById('message').innerHTML = jsonArray.messages[j].text;
-                  j++;                 
-                  if (j < jsonArray.messages.length) {
-                    myLoop();
-                  }
-                  else if(j == jsonArray.messages.length){
-                      j = 0;
-                      myLoop();
-                  }              
-                }, jsonArray.messages[j].duration*1000)
-              }
-            myLoop();
-        }
+let counter = 0;
+function myLoop(jsonArray) {
+  setTimeout(function () {
+    document.getElementById("message").innerHTML = jsonArray.messages[counter].text;
+    counter++;
+    if (counter < jsonArray.messages.length) {
+      myLoop(messages);
+    } else if (counter == jsonArray.messages.length) {
+      counter = 0;
+      myLoop(messages);
     }
+  }, jsonArray.messages[counter].duration * 1000);
 }
-
-displayMessage(messages);
+myLoop(messages);
